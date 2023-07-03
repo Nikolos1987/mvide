13b7a9da7498854e1697110a6222c25a13ef4169
Nikita <nikolos04041987@gmail.com>
2023-07-03 13:33:40 +0700

    cards

--- my-app/src/components/main.tsx
+++ my-app/src/components/main.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from "react";
-import { Cards } from "./productCard";
+import {Cards} from "./productCard"
 export function Main() {
 
     return (
--- my-app/src/components/productCard.tsx
+++ my-app/src/components/productCard.tsx
@@ -1,10 +1,15 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { log } from "console"
+import { useEffect,useState } from "react"
 
 export function Cards (){
-    useEffect(()=>{axios.get("http://localhost:3000/server/data.json").then(res=>console.log(res.data))},[])
+    const [data,setData]=useState<any>("")
+    useEffect(()=>{axios.get("http://localhost:3000/server/data.json").then(res=>{setData([...data,res.data])}
+    
+    )},[])
+console.log(data,"this")
 
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res=>console.log(res))
+
     return(
         <div className="w-64 h-72 color bg-red-900 rounded-lg"></div>
     )
