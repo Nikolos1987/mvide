7a9db66063929eefbfc3cfa37bde9a1e0205ae3d
Nikita <nikolos04041987@gmail.com>
2023-07-13 15:45:59 +0700

    13.07

--- my-app/src/components/menu.tsx
+++ my-app/src/components/menu.tsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { countPlus } from "../store/profile";
 export function Menu() {
 
-    const data: any = useSelector((st: { profiles: {}, data: { data: string, num: number } }): { profiles: {}, data: { data: string, num: number } } => st)
+    const data:any= useSelector((st: { profiles: {}, data: { data: string, num: number } }): { profiles: {}, data: { data: string, num: number } } => st)
     const dispatch = useDispatch()
 
     const cnt = () => {
@@ -11,7 +11,7 @@ export function Menu() {
     return (
         <div style={{ height: '1000px' }} className="bg-slate-500 flex ">
 
-                   <div className="bg-slate-300 w-1/3 pt-36">
+            <div className="bg-slate-300 w-1/3 pt-36">
                 <ul>
                     <li className="cursor-pointer hover:text-red-600">{data.profiles.data}
                     </li>
